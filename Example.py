e8492a7a46886ab862723cd1dcb320e599474105
biva8710 <s06181110@gmail.com>
2019-12-07 13:36:10 +0900

    few refactor

--- Example.py
+++ Example.py
@@ -18,14 +18,20 @@ write_list = []
 with open(a_file, 'r', encoding='utf-8') as a_file:
     counter = 0
     for a_string in a_file:
+        # a_string = Jul 12  2019 22:24:46,25,読書,To Do
         if counter < 2:
             counter += 1
             continue
         a_list = a_string.split(',')
-        start_time = datetime.datetime.strptime(a_list[0], '%b %d %Y %H:%M:%S')
-        if target_day < start_time:
-            end_time = start_time + datetime.timedelta(minutes=int(a_list[1]))
-            write_list.append([a_list[2], ','.join(str(start_time).split()), ','.join(str(end_time).split())])
+        # a_list = [Jul 12  2019 22:24:46, 25, 読書, To Do]
+        start_date = datetime.datetime.strptime(a_list[0], '%b %d %Y %H:%M:%S')
+        # start_date = 2019-10-14 12:11:45
+        if target_day < start_date:
+            end_date = start_date + datetime.timedelta(minutes=int(a_list[1]))
+            end_date, end_time = str(end_date).split()
+            subject = a_list[2]
+            start_date, start_time = str(start_date).split()
+            write_list.append([subject, start_date, start_time, end_date, end_time])
 
 a_file = os.path.join(home_directory, 'Desktop', 'BeFocused.csv')
 with open(a_file, 'w', encoding='utf-8') as a_file:
